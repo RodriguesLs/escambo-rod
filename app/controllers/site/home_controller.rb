e78f3f619b5850af396315c77873e503e29658c4
Rodrigues <fr.costa95@gmail.com>
2019-01-07 23:33:40 +0000

    testing ssh key

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -3,5 +3,6 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order(:description)
     @ads = Ad.descending_order().page(params[:page]).per(6)
+    @carousel = Ad.limit(3).order("RANDOM()")
   end
 end

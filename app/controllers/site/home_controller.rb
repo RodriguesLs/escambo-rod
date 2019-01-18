2b8a706ab6b595d87f135568e0ab5943a5d63d43
Rodrigues <fr.costa95@gmail.com>
2019-01-18 21:33:28 +0000

    Fix method rand() to MySQL

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -3,6 +3,11 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order(:description)
     @ads = Ad.descending_order().page(params[:page]).per(6)
-    @carousel = Ad.limit(3).order("RANDOM()")
+    
+    if Rails.env.production?  
+      @carousel = Ad.limit(3).order("RAND()")
+    else
+      @carousel = Ad.limit(3).order("RANDOM()")
+    end
   end
 end

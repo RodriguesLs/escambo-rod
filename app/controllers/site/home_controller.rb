cc093ede823173bcc0cc20db08abf7d2b62dcea8
Rodrigues <fr.costa95@gmail.com>
2019-01-18 21:44:43 +0000

    Adjusting method rand()

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,13 +1,13 @@
 class Site::HomeController < SiteController
-  
   def index
+    
     @categories = Category.order(:description)
     @ads = Ad.descending_order().page(params[:page]).per(6)
     
-    if Rails.env.production?  
+    #if Rails.env.production?  
       @carousel = Ad.limit(3).order("RAND()")
-    else
-      @carousel = Ad.limit(3).order("RANDOM()")
-    end
+    #else
+    #  @carousel = Ad.limit(3).order("RANDOM()")
+    #end
   end
 end

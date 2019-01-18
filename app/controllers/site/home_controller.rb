e145aa4f900b33b4e27e6b063a9826c85b9fe047
Rodrigues <fr.costa95@gmail.com>
2019-01-18 22:58:14 +0000

    Modify home_controller again to test

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,6 +2,6 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order(:description)
     @ads = Ad.descending_order().page(params[:page]).per(6)
-    @carousel = Ad.random(3)
+    @carousel = Ad.limit(3).order("RAND()")
   end
 end

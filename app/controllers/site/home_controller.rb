16e4818e3ccfaa34d34aa6bf174a7dab2c8efe5c
Rodrigues <fr.costa95@gmail.com>
2019-01-18 22:45:15 +0000

    trying scope again

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,11 +2,6 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order(:description)
     @ads = Ad.descending_order().page(params[:page]).per(6)
-    
-    if Rails.env.production?
-      @carousel = Ad.limit(3).order("RAND()")
-    else
-      @carousel = Ad.limit(3).order("RANDOM()")
-    end
+    @carousel = Ad.random(3)
   end
 end
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -14,6 +14,14 @@ class Ad < ActiveRecord::Base
   scope :descending_order, -> () {order(created_at: :desc)}
   scope :search, -> (q, page) { where("title LIKE ?", "%#{q}%").page(page).per(6) }
   
+  scope :random, ->(quantity) {
+    if Rails.env.production?
+      limit(quantity).order("RAND()") # MySQL
+    else
+      limit(quantity).order("RANDOM()") # SQLite
+    end
+  }  
+  
   has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100>" }, 
                                       default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/

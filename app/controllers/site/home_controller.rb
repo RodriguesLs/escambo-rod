2adc9d7a6abf4dbb61297178ee9a35300b1de025
Rodrigues <fr.costa95@gmail.com>
2019-01-20 14:54:28 +0000

    Rolback to Rails.env.production?

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,6 +2,10 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order(:description)
     @ads = Ad.descending_order().page(params[:page]).per(6)
-    @carousel = Ad.limit(3).order("RAND()")
+    if Rails.env.production?
+      @carousel = Ad.limit(3).order("RAND()")
+    else
+      @carousel = Ad.limit(3).order("RANDOM()")
+    end
   end
 end
